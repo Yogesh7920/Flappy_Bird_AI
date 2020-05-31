0167218fe47306470aeedb42a427bf5f3f49b1e1
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2020-05-31 18:24:53 +0530

    Add the local project to github. Modified README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flappy_Bird_AI
+The AI that never loses in Flappy_Bird
+
+
+To see the training run main.py
+
+To evaluate the best run eval.py
+
+##### HAVE FUN TWEAKING AND WATCHING THE AI CONQUER THE GAME.
---
+++ eval.py
@@ -0,0 +1,141 @@
+import pygame
+import neat
+import os
+from bird import Bird
+from pipe import Pipe
+from base import Base
+import pickle
+pygame.font.init()
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+BG_IMG = pygame.image.load(os.path.join('imgs', 'bg.png'))
+BG_IMG = pygame.transform.scale(BG_IMG, (WIN_WIDTH, 900))
+
+STAT_FONT = pygame.font.SysFont('comicsans', 50)
+
+
+def draw_window(win, birds, pipes, base, score, gen):
+    win.blit(BG_IMG, (0, 0))
+
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render('Score: ' + str(score), 1, (255, 255, 255))
+    win.blit(text, (WIN_WIDTH-10 - text.get_width(), 10))
+    text = STAT_FONT.render('GEN: ' + str(gen), 1, (255, 255, 255))
+    win.blit(text, (10, 10))
+
+    base.draw(win)
+    for bird in birds:
+        bird.draw(win)
+    pygame.display.update()
+
+
+gen = 1
+
+
+def main(genomes, config):
+    global gen
+    nets = []
+    ge = []
+    birds = []
+
+    for g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
+    base = Base(730)
+    pipes = [Pipe(700)]
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+    score = 0
+    run = True
+
+    while run:
+        clock.tick(50)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+
+        if score > 100:
+            run = False
+            break
+
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y-pipes[pipe_ind].height),
+                                       abs(bird.y-pipes[pipe_ind].bottom)))
+            if output[0] > 0.5:
+                bird.jump()
+
+        rem = []
+        add_pipe = False
+        for pipe in pipes:
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            for genome in ge:
+                genome.fitness += 5
+            pipes.append(Pipe(600))
+
+        for r in rem:
+            pipes.remove(r)
+
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+
+        base.move()
+        draw_window(win, birds, pipes, base, score, gen)
+
+    gen += 1
+
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+
+    with open('best.pickle', 'rb') as f:
+        winner = pickle.load(f)
+
+    main([winner], config)
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)
+
